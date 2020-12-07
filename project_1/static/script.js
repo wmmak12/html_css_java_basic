42d77df722ebcad06065070ea72a5a05e9e5308c
Ethan Mak <ethan.mak@streetsine.com>
2020-12-07 19:16:33 +0800

    challenge 4

--- project_1/static/script.js
+++ project_1/static/script.js
@@ -87,3 +87,58 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
     document.getElementById('flex-box-rps-id').appendChild(messageDiv)
     document.getElementById('flex-box-rps-id').appendChild(botDiv)
 }
+
+
+// Challenge 4: Change the Color of all buttons
+var all_buttons = document.getElementsByTagName('button');
+
+var copyAllButtons = [];
+for (let i=0; i < all_buttons.length; i++) {
+    copyAllButtons.push(all_buttons[i].classList[1])
+}
+
+function buttonColorChange(buttonThingy) {
+    if (buttonThingy.value === 'red') {
+        buttonsRed();
+    } else if (buttonThingy.value === 'green') {
+        buttonsGreen();
+    } else if (buttonThingy.value === 'reset') {
+        buttonColorReset();
+    } else if (buttonThingy.value === 'random') {
+        randomColors();
+    }
+}
+
+function buttonsRed() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add('btn-danger')
+    }
+}
+
+function buttonsGreen() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add('btn-success')
+    }
+}
+
+function buttonColorReset() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add(copyAllButtons[i])
+    }
+}
+
+function randomColors() {
+    let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
+    for (let i = 0; i < all_buttons.length; i++) {
+        let randomNum = Math.floor(Math.random() * 4)
+        all_buttons[i].classList.remove(all_buttons[i].classList[1])
+        all_buttons[i].classList.add(choices[randomNum])
+    }
+}
+
+
+// Challenge 5 How Blackjack works
+
